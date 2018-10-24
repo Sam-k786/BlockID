a1349ca947bd89dfd7abe2579bcbf6db02b192b6
Ayush <ayushkgupta007@gmail.com>
2018-10-25 01:07:16 +0530

    server files

---
+++ server/routes/routes.js
@@ -0,0 +1,13 @@
+// const express = require("express");
+// const router = express.Router();
+// const users = require("../models/users.js");
+// const mongoose = require("mongoose");
+// const bcrypt = require("bcrypt");
+// const nodemailer = require('nodemailer');
+// const request = require('request');
+// const jwt = require('jsonwebtoken');
+// const checkAuth = require('../middleware/checkAuth.js');
+// const config = require('../config.json');
+// const web3 = require('web3');
+
+var web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
