21baf0855d6c21bd131a735d9cf740e4f59de4fa
Ayush <ayushkgupta007@gmail.com>
2018-10-27 03:08:37 +0530

    signup linked

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,269 +1,211 @@
-
 $(document).ready(function(){
+    var sign,msgParams;
+    if (typeof web3 !== 'undefined') {
+        // Use Mist/MetaMask's provider
+        web3 = new Web3(web3.currentProvider);
+    } else {
+        console.log('No web3? You should consider trying MetaMask!')
+        // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+        //web3js = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+    }
+    /* Validation part */
+    $('#address_error').hide();
+    $('#email_error').hide();
+    $('#phone_error').hide();
+    $('#password_error').hide();
+    $('#country_error').hide();
 
-	/*
-  	Validation part
-  */
-  		$('#address_error').hide();
-        $('#email_error').hide();
-        $('#phone_error').hide();
-        $('#password_error').hide();
-        $('#country_error').hide();
-        
-        var address_error = false;
-        var name_error = false;
-        var email_error = false;
-        var phone_error = false;
-        var password_error = false;
-        var country_error = false;
-        $('#name').focusout(function(){
-        	check_name();
-        });
-        $('#email').focusout(function(){
-            check_email();
-        });
-        $('#phone').focusout(function(){
-            check_phone();
-        });
-        $('#password, #password2').focusout(function(){
-            check_password();
-        });
-        $('#country_error').focusout(function(){
-            check_country();
-        });
+    var address_error = false;
+    var name_error = false;
+    var email_error = false;
+    var phone_error = false;
+    var password_error = false;
+    var country_error = false;
+    $('#name').focusout(function(){
+        check_name();
+    });
+    $('#email').focusout(function(){
+        check_email();
+    });
+    $('#phone').focusout(function(){
+        check_phone();
+    });
+    $('#password, #password2').focusout(function(){
+        check_password();
+    });
+    $('#country_error').focusout(function(){
+        check_country();
+    });
 
-        function check_name() {
-        	var name = $('#name').val();
-        	//name.trim();
-        	if(name!='') {
-        		$('#name_error').hide();
-        	}
-        	else {
-        		$('#name_error').html("Name field cannot be empty");
-        		$('#name_error').show();
-        		name_error=true;	
-        	}
+    function check_name() {
+        var name = $('#name').val();
+        //name.trim();
+        if(name!='') {
+            $('#name_error').hide();
         }
-        function check_email() {
-            var pattern = /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@(([0-9a-zA-Z])+([-\w]*[0-9a-zA-Z])*\.)+[a-zA-Z]{2,9})$/;
-            var email = $('#email').val().trim();
-            if(pattern.test(email)&&email!='') {
-                $('#email_error').hide();
+        else {
+            $('#name_error').html("Name field cannot be empty");
+            $('#name_error').show();
+            name_error=true;
+        }
+    }
+    function check_email() {
+        var pattern = /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@(([0-9a-zA-Z])+([-\w]*[0-9a-zA-Z])*\.)+[a-zA-Z]{2,9})$/;
+        var email = $('#email').val().trim();
+        if(pattern.test(email)&&email!='') {
+            $('#email_error').hide();
+        }
+        else {
+            $('#email_error').html("Invalid email address");
+            $('#email_error').show();
+            email_error=true;
+        }
+    }
+    function check_phone() {
+        var pattern = /^((\+[1-9]{1,4}[ \-]*)|(\([0-9]{2,3}\)[ \-]*)|([0-9]{2,4})[ \-]*)*?[0-9]{3,4}?[ \-]*[0-9]{3,4}?$/;
+        var phone = $('#phone').val().trim();
+        if(pattern.test(phone)&&phone!='') {
+            $('#phone_error').hide();
+        }
+        else {
+            $('#phone_error').html("Invalid phone number");
+            $('#phone_error').show();
+            phone_error=true;
+        }
+    }
+    function check_password() {
+        var password = $('#password').val();
+        var password2 = $('#password2').val();
+        if(password!='' && password2!='') {
+            if(password == password2) {
+                $('#password_error').hide();
             }
             else {
-                $('#email_error').html("Invalid email address");
-                $('#email_error').show();
-                email_error=true;
-            }
-        }
-        function check_phone() {
-            var pattern = /^((\+[1-9]{1,4}[ \-]*)|(\([0-9]{2,3}\)[ \-]*)|([0-9]{2,4})[ \-]*)*?[0-9]{3,4}?[ \-]*[0-9]{3,4}?$/;
-            var phone = $('#phone').val().trim();
-            if(pattern.test(phone)&&phone!='') {
-                $('#phone_error').hide();
-            }
-            else {
-                $('#phone_error').html("Invalid phone number");
-                $('#phone_error').show();
-                phone_error=true;
-            }
-        }
-        function check_password() {
-            var password = $('#password').val();
-            var password2 = $('#password2').val();
-            if(password!='' && password2!='') {
-                if(password == password2) {
-                    $('#password_error').hide();
-                }
-                else {
-                    $('#password_error').html("Passwords do not match");
-                    $('#password_error').show();
-                    password_error=true;
-                }
-            }
-            
-        }
-        function check_country() {
-            var country = $('#country').val();
-            var temp=country.trim();
-            if(temp!='') {
-                $('#country_error').hide();
-            }
-            else {
-                $('#country_error').html("Enter country name");
-                $('#country_error').show();
-                country_error=true;
+                $('#password_error').html("Passwords do not match");
+                $('#password_error').show();
+                password_error=true;
             }
         }
 
-        /*
-        	Validation ends...
-        */
+    }
+    function check_country() {
+        var country = $('#country').val();
+        var temp=country.trim();
+        if(temp!='') {
+            $('#country_error').hide();
+        }
+        else {
+            $('#country_error').html("Enter country name");
+            $('#country_error').show();
+            country_error=true;
+        }
+    }
+    /* Validation ends */
 
-
-  $(".form-wrapper .button").click(function(e){
-    
-    e.preventDefault();
-                    
-    var button = $(this);
-    var currentSection = button.parents(".section");
-    var currentSectionIndex = currentSection.index();
-    var account_add = $('#account_add').val();
-    var valid_add = false;
-    // if(currentSectionIndex==0) {
-    // 	$.ajax({
-    // 		type: "POST",
-    // 		url: "www.google.com",
-    // 		data: { address : account_add },
-    // 		success: function() {
-    // 			alert("address sent!");
-    // 		},
-    // 		error: function() {
-    // 			alert("error");
-    // 		}
-    // 	});
-    // }
-    // else {
-    // 	var headerSection = $('.steps li').eq(currentSectionIndex);
-	   //  currentSection.removeClass("is-active").next().addClass("is-active");
-	   //  headerSection.removeClass("is-active").next().addClass("is-active");
-
-	   //  $(".form-wrapper").submit(function(e) {
-	   //    e.preventDefault();
-	   //  });
-
-	   //  if(currentSectionIndex === 2){
-	   //    $(document).find(".form-wrapper .section").first().addClass("is-active");
-	   //    $(document).find(".steps li").first().addClass("is-active");
-	   //  }
-    // }
-
-    	if(currentSectionIndex==0) {
-    		// $.ajax({
-	    	// 	type: "POST",
-	    	// 	url: "www.google.com",
-	    	// 	data: { address : account_add },
-	    	// 	success: function() {
-	    	// 		alert("address sent!");
-	    	// 	},
-	    	// 	error: function() {
-	    	// 		alert("error");
-	    	// 	}
-    		// });
-
-    		//remove below line when calls work
-    		valid_add=true;
-    		if(valid_add) {
-    			var headerSection = $('.steps li').eq(currentSectionIndex);
-				    currentSection.removeClass("is-active").next().addClass("is-active");
-				    headerSection.removeClass("is-active").next().addClass("is-active");
-
-				    
-
-				    if(currentSectionIndex === 2){
-				      $(document).find(".form-wrapper .section").first().addClass("is-active");
-				      $(document).find(".steps li").first().addClass("is-active");
-				    }
-    		}
-
-    	}
-    	else {
-    		var headerSection = $('.steps li').eq(currentSectionIndex);
-		    currentSection.removeClass("is-active").next().addClass("is-active");
-		    headerSection.removeClass("is-active").next().addClass("is-active");
-
-		    
-		    if(currentSectionIndex === 2) {
-		      $(document).find(".form-wrapper .section").first().addClass("is-active");
-		      $(document).find(".steps li").first().addClass("is-active");
-		    }
-		    if(currentSectionIndex==1) {
-		    	$('.submit').click(function() {
-		    		email_error=false;
-		    		password_error=false;
-		    		phone_error=false;
-		    		check_email();
-		    		check_password();
-		    		check_phone();
-                    check_country();
-		    		if(!name_error & !email_error && !password_error && !phone_error && !country_error) {
-		    			var account_add = $('#account_add').val();
-		    			var name  = $('#name').val();
-		    			var email = $('#email').val();
-		    			var password = $('#password').val();
-		    			var phone = $('#phone').val();
-                        var country = $('#country').val();
-		    			$.ajax({
-		    				type:'POST',
-		    				url: '',
-		    				data: {account_add: account_add, name: name, email: email, password: password,phone: phone,country: country},
-		    				success: function() {
-		    					alert("Values sent!");
-		    				},
-		    				error: function() {
-		    					alert("Error :(");
-		    				}
-		    			});
-		    		}
-		    	});
-		    }
-    	}
-    
-  });
-
-
-
-  
-
-
-        
-
-
-    //     $('.mailbtn').click(function() {
-    //         name_error=false;
-    //         email_error=false;
-    //         phone_error=false;
-    //         message_error=false;
-    //         check_name();
-    //         check_email();
-    //         check_phone();
-    //         check_message();
-    //         if(name_error==false && email_error==false && phone_error==false && message_error==false) {
-    //             var name = $('#name').val();
-    //             var email = $('#email').val();
-    //             var phone = $('#phone').val();
-    //             var message = $('#message').val();
-    //             $('.result').css("display","block");
-                    
-    //             $.ajax({
-    //                 type: "POST",
-    //                 url:'form_action.php',
-    //                 data: {name : name, email : email, phone : phone, message : message},
-    //                 success: function() {
-				// 	alert("Message Sent!");
-				// }
-    //             });
-    //         }
-    //         else {
-    //             alert("Please fill the form correctly.");
-    //         }
-    //     });
-
-
-    /*
-		Random Fingerprint
-    */
+    /* Random Fingerprint */
     function randomFingerprint() {
-	  var fingerprint = "";
-	  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        var fingerprint = "";
+        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-	  for (var i = 0; i < 32; i++)
-	    fingerprint += possible.charAt(Math.floor(Math.random() * possible.length));
+        for (var i = 0; i < 32; i++)
+        fingerprint += possible.charAt(Math.floor(Math.random() * possible.length));
 
-	  return fingerprint;
-	}
-	$('#fingerprint').val(randomFingerprint());
+        return fingerprint;
+    }
+    $('#fingerprint').val(randomFingerprint());
 
+    $(".form-wrapper .button").click(function(e){
+
+        e.preventDefault();
+        var button = $(this);
+        var currentSection = button.parents(".section");
+        var currentSectionIndex = currentSection.index();
+        var account_add = $('#account_add').val();
+        var password = $('#password').val();
+        check_password();
+        if(password_error){
+            return;
+        }
+        if(currentSectionIndex==0) {
+            msgParams = [
+                {
+                    type: 'bytes32',      // Any valid solidity type
+                    name: 'Message',     // Any string label you want
+                    value: password
+                }
+            ]
+            var params = [msgParams, account_add]
+            var method = 'eth_signTypedData'
+            web3.currentProvider.sendAsync({
+                method,
+                params,
+                account_add
+            }, function (err, result) {
+                if (err) return console.error(err)
+                if (result.error) return console.error(result.error)
+                sign = result.result;
+            });
+            var headerSection = $('.steps li').eq(currentSectionIndex);
+            currentSection.removeClass("is-active").next().addClass("is-active");
+            headerSection.removeClass("is-active").next().addClass("is-active");
+        }
+        else {
+            var headerSection = $('.steps li').eq(currentSectionIndex);
+            currentSection.removeClass("is-active").next().addClass("is-active");
+            headerSection.removeClass("is-active").next().addClass("is-active");
+            if(currentSectionIndex==1) {
+                $('.submit').click(function() {
+                    email_error=false;
+                    password_error=false;
+                    phone_error=false;
+                    check_email();
+                    check_password();
+                    check_phone();
+                    check_country();
+                    if(!name_error & !email_error && !password_error && !phone_error && !country_error) {
+                        var account_add = $('#account_add').val();
+                        var name  = $('#name').val();
+                        var email = $('#email').val();
+                        var password = $('#password').val();
+                        var phone = $('#phone').val();
+                        var country = $('#country').val();
+                        var fingerprint = $('#fingerprint').val();
+                        var dob = $('#dob').val();
+                        var perAddr = $('#address').val();
+                        var finalResult = {
+                            signature : sign,
+                            address:account_add,
+                            password:password,
+                            fingerprint: fingerprint,
+                            email:email,
+                            phone:phone,
+                            perAddr:perAddr,
+                            country:country,
+                            name:name,
+                            dob:dob,
+                            msgParams:msgParams
+
+                        };
+                        $.ajax({
+                            type:"POST",
+                            url:"http://localhost:3000/signup",
+                            data:finalResult,
+                            success: function(res){
+                                if(res.status === 'success'){
+                                    alert(res.message);
+                                    location.reload(true);
+                                }else{
+                                    alert(res.message);
+                                }
+                            },
+                            error: function(err){
+                                alert('Some error occured.Please try again.');
+                                location.reload(true);
+                            }
+                        });
+                    }
+                });
+            }
+        }
+    });
 });
-
-
